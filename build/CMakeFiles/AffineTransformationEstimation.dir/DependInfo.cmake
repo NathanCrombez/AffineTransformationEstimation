eb515373796dc03fe5859e331770f2b0fd94712a
NathanCrombez <nathan.crombez@gmail.com>
2018-06-01 14:29:30 +0200

    Name change

---
+++ build/CMakeFiles/AffineTransformationEstimation.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nathan/Developpements/AffineTransformationEstimation/main.cpp" "/home/nathan/Developpements/AffineTransformationEstimation/build/CMakeFiles/AffineTransformationEstimation.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/opencv"
+  "/usr/include/libxml2"
+  "/usr/include/libusb-1.0"
+  "/opt/ros/indigo/include"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/vtk-5.8"
+  "/usr/include/OGRE"
+  "/usr/include/ois"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/AffineTransformationEstimation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/AffineTransformationEstimation.dir/main.cpp.o"
+  "AffineTransformationEstimation.pdb"
+  "AffineTransformationEstimation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/AffineTransformationEstimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
