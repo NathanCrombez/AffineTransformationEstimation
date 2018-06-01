38b12d872ce732828467a1b09c540f2ae21b76a4
NathanCrombez <nathan.crombez@gmail.com>
2018-06-01 14:20:58 +0200

    first commit

---
+++ build/CMakeFiles/2DAlignement.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/2DAlignement.dir/main.cpp.o"
+  "2DAlignement.pdb"
+  "2DAlignement"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/2DAlignement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
